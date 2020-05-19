7f1d539c7a52f65fcf252af90c18981ba6b13294
Rebecca Zanzig <16315901+adilyse@users.noreply.github.com>
2020-05-19 11:19:25 -0700

    Update server acl init to create tokens for gateways
    This allows the server acl init job to create base tokens for any
    provided ingress and terminating gateway names.
    
    For terminating gateways, users will need to update these created
    tokens to include permissions to the services they represent. To
    do this, they should add separate policies or attach service identities
    to avoid having this job overwrite their configuration.
    
    When Consul namespaces are enabled, users can provide a namespace with
    each gateway name to specify which Consul namespace to scope the gateway
    token to. In this case, the gateway flag values can be provided as
    `<gateway-name>.<gateway-namespace>`. If a namespace is not provided,
    the token will be created for the `default` namespace.

--- subcommand/server-acl-init/command.go
+++ subcommand/server-acl-init/command.go
@@ -49,7 +49,9 @@ type Command struct {
 
 	flagCreateSnapshotAgentToken bool
 
-	flagCreateMeshGatewayToken bool
+	flagCreateMeshGatewayToken  bool
+	flagIngressGatewayNames     []string
+	flagTerminatingGatewayNames []string
 
 	// Flags to configure Consul connection
 	flagServerAddresses     []string
@@ -124,6 +126,14 @@ func (c *Command) init() {
 		"[Enterprise Only] Toggle for creating a token for the Consul snapshot agent deployment.")
 	c.flags.BoolVar(&c.flagCreateMeshGatewayToken, "create-mesh-gateway-token", false,
 		"Toggle for creating a token for a Connect mesh gateway.")
+	c.flags.Var((*flags.AppendSliceValue)(&c.flagIngressGatewayNames), "ingress-gateway-name",
+		"Name of an ingress gateway that needs an acl token. May be specified multiple times. "+
+			"[Enterprise Only] If using Consul namespaces and registering the gateway outside of the "+
+			"default namespace, specify the value in the form <GatewayName>.<ConsulNamespace>.")
+	c.flags.Var((*flags.AppendSliceValue)(&c.flagTerminatingGatewayNames), "terminating-gateway-name",
+		"Name of a terminating gateway that needs an acl token. May be specified multiple times. "+
+			"[Enterprise Only] If using Consul namespaces and registering the gateway outside of the "+
+			"default namespace, specify the value in the form <GatewayName>.<ConsulNamespace>.")
 
 	c.flags.Var((*flags.AppendSliceValue)(&c.flagServerAddresses), "server-address",
 		"The IP, DNS name or the cloud auto-join string of the Consul server(s). If providing IPs or DNS names, may be specified multiple times."+
@@ -488,6 +498,108 @@ func (c *Command) Run(args []string) int {
 		if err != nil {
 			c.log.Error(err.Error())
 			return 1
+		}
+	}
+
+	if len(c.flagIngressGatewayNames) > 0 {
+		// Create a token for each ingress gateway name. Each gateway needs a
+		// separate token because users may need to attach different policies
+		// to each gateway token depending on what the services it represents
+		for _, name := range c.flagIngressGatewayNames {
+			if name == "" {
+				c.log.Error("Ingress gateway names cannot be empty")
+				return 1
+			}
+
+			// Parse optional namespace. This does not protect against a user
+			// that provides a namespace when not enabling namespaces.
+			var namespace string
+			if c.flagEnableNamespaces {
+				parts := strings.SplitN(strings.TrimSpace(name), ".", 2)
+				if len(parts) > 1 {
+					// Name and namespace were provided
+					name = parts[0]
+
+					// Use default namespace if provided flag is of the
+					// form "name."
+					if parts[1] != "" {
+						namespace = parts[1]
+					} else {
+						namespace = "default"
+					}
+				} else {
+					// Use the default Consul namespace
+					namespace = "default"
+				}
+			}
+
+			// Define the gateway rules
+			ingressGatewayRules, err := c.ingressGatewayRules(name, namespace)
+			if err != nil {
+				c.log.Error("Error templating ingress gateway rules", "gateway-name", name,
+					"namespace", namespace, "err", err)
+			}
+
+			// The names in the Helm chart are specified by users and so may not contain
+			// the words "ingress-gateway". We need to create unique names for tokens
+			// across all gateway types and so must suffix with `-ingress-gateway`.
+			tokenName := fmt.Sprintf("%s-ingress-gateway", name)
+			err = c.createLocalACL(tokenName, ingressGatewayRules, consulDC, consulClient)
+			if err != nil {
+				c.log.Error(err.Error())
+				return 1
+			}
+		}
+	}
+
+	if len(c.flagTerminatingGatewayNames) > 0 {
+		// Create a token for each terminating gateway name. Each gateway needs a
+		// separate token because users may need to attach different policies
+		// to each gateway token depending on what the services it represents
+		for _, name := range c.flagTerminatingGatewayNames {
+			if name == "" {
+				c.log.Error("Terminating gateway names cannot be empty")
+				return 1
+			}
+
+			// Parse optional namespace. This does not protect against a user
+			// that provides a namespace when not enabling namespaces.
+			var namespace string
+			if c.flagEnableNamespaces {
+				parts := strings.SplitN(strings.TrimSpace(name), ".", 2)
+				if len(parts) > 1 {
+					// Name and namespace were provided
+					name = parts[0]
+
+					// Use default namespace if provided flag is of the
+					// form "name."
+					if parts[1] != "" {
+						namespace = parts[1]
+					} else {
+						namespace = "default"
+					}
+				} else {
+					// Use the default Consul namespace
+					namespace = "default"
+				}
+			}
+
+			// Define the gateway rules
+			terminatingGatewayRules, err := c.terminatingGatewayRules(name, namespace)
+			if err != nil {
+				c.log.Error("Error templating terminating gateway rules", "gateway-name", name,
+					"namespace", namespace, "err", err)
+			}
+
+			// The names in the Helm chart are specified by users and so may not contain
+			// the words "ingress-gateway". We need to create unique names for tokens
+			// across all gateway types and so must suffix with `-terminating-gateway`.
+			tokenName := fmt.Sprintf("%s-terminating-gateway", name)
+			err = c.createLocalACL(tokenName, terminatingGatewayRules, consulDC, consulClient)
+			if err != nil {
+				c.log.Error(err.Error())
+				return 1
+			}
 		}
 	}
 
